577fc3a81492933cb901cfa2c6da076abf635cfe
azuki <azukibeans.5kilo@gmail.com>
2018-11-20 00:31:33 +0900

    added renderer of editor component

---
+++ src/renderer/components/Editor.vue
@@ -0,0 +1,24 @@
+<template>
+  <HotTable :settings="config" ref="hotTableComponent"/>
+</template>
+
+<script>
+import Handsontable from 'handsontable'
+import { HotTable } from '@handsontable/vue'
+
+export default {
+  data() {
+    return {
+      config: {
+        data: Handsontable.helper.createSpreadsheetData(6, 6),
+        colHeaders: true
+      }
+    }
+  },
+
+  components: { HotTable }
+}
+</script>
+
+<style src="@/../../node_modules/handsontable/dist/handsontable.full.css">
+</style>
