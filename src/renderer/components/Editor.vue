921d6b02d29cb4af5648feb6f4747525263b3f4b
azuki <azukibeans.5kilo@gmail.com>
2018-11-20 15:36:54 +0900

    visualize data to grid when csv file opened

--- src/renderer/components/Editor.vue
+++ src/renderer/components/Editor.vue
@@ -3,12 +3,23 @@
 </template>
 
 <script>
+import Encoder from 'encoding-japanese'
+import Papa from 'papaparse'
 import Handsontable from 'handsontable'
 import { HotTable } from '@handsontable/vue'
+import { ipcRenderer } from 'electron'
 
 export default {
   data() {
     return {
+      file: {
+        bytes:   null,
+        text:    null,
+        path:    null,
+        encode:  'UTF8',
+        newline: 'NL'
+      },
+
       config: {
         data: Handsontable.helper.createSpreadsheetData(6, 6),
         colHeaders: true
@@ -16,7 +27,23 @@ export default {
     }
   },
 
-  components: { HotTable }
+  components: { HotTable },
+
+  mounted() {
+    ipcRenderer.on('send-file-contents', (e, file_info) => {
+      this.file.bytes  = file_info.bytes
+      this.file.path   = file_info.path
+      this.file.encode = Encoder.detect(this.file.bytes)
+      this.file.text   = Encoder.convert(this.file.bytes, {
+        from: this.file.encode,
+        to:   'UNICODE',
+        type: 'string'
+      })
+
+      let table = this.$refs.hotTableComponent.hotInstance
+      table.loadData(Papa.parse(this.file.text.trim()).data)
+    })
+  }
 }
 </script>
 
